472cd78503297b1a46c796704260449bfd12f315
kmorris72 <kipp.morris@gmail.com>
2018-04-01 21:57:24 -0400

    Adding padding between widgets in LoginWindow.py

--- LoginWindow.py
+++ LoginWindow.py
@@ -8,41 +8,46 @@ class LoginWindow:
         self.label = Label(master,
                            text="ATL Gardens, Farms, and Orchards",
                            font="Times 48")
-        self.label.pack()
+        self.label.pack(pady=(0, 30))
 
-        self.email_container = Frame(master)
-        self.email_container.pack()
-        self.email_label = Label(self.email_container,
+        self.email_password_container = Frame(master)
+        self.email_password_container.pack(pady=(0, 20))
+
+        self.label_container = Frame(self.email_password_container)
+        self.label_container.pack(side=LEFT)
+
+        self.text_entry_container = Frame(self.email_password_container)
+        self.text_entry_container.pack(side=RIGHT)
+
+        self.email_label = Label(self.label_container,
                                  text="Email:",
                                  font="Times 16")
-        self.email_label.pack(side=LEFT)
-        self.email_text = Entry(self.email_container,
+        self.email_label.pack(side=TOP)
+        self.email_text = Entry(self.text_entry_container,
                                 font="Times 16",
                                 width=30)
-        self.email_text.pack(side=LEFT)
+        self.email_text.pack(side=TOP)
 
-        self.password_container = Frame(master)
-        self.password_container.pack()
-        self.password_label = Label(self.password_container,
+        self.password_label = Label(self.label_container,
                                     text="Password:",
                                     font="Times 16")
-        self.password_label.pack(side=LEFT)
-        self.password_text = Entry(self.password_container,
+        self.password_label.pack(side=BOTTOM)
+        self.password_text = Entry(self.text_entry_container,
                                    font="Times 16",
                                    width=30)
-        self.password_text.pack(side=LEFT)
+        self.password_text.pack(side=BOTTOM)
 
         self.login_button = Button(master,
                                    text="Login",
                                    padx=10)
-        self.login_button.pack()
+        self.login_button.pack(pady=(0, 30))
 
         self.reg_button_container = Frame(master)
-        self.reg_button_container.pack()
+        self.reg_button_container.pack(pady=(0, 30))
         self.owner_reg_button = Button(self.reg_button_container,
                                        text="New Owner Registration",
                                        padx=10)
-        self.owner_reg_button.pack(side=LEFT)
+        self.owner_reg_button.pack(side=LEFT, padx=(0, 50))
         self.visitor_reg_button = Button(self.reg_button_container,
                                          text="New Visitor Registration",
                                          padx=10)
