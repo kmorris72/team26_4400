5746866671de531048568a01e3669f5f75dba2d3
Stephen Wist <stphnwist@gmail.com>
2018-04-13 22:00:55 -0400

    searching/sorting. still broken for searching by visits

--- VisitorHome.py
+++ VisitorHome.py
@@ -14,7 +14,12 @@ Add ability to search by visit number range or avg rating range
 Add ability to select a property
 Add "View Property" button that goes to that propertie's page
 Add "View Visit History" and function
+
+CURRENTLY DOING
+fixing the search on visits or avg visits
 '''
+
+ATTRS = 'Name, ID, Street, Size, City, Zip, PropertyType, IsPublic, IsCommercial'
 
 # This class is the whole window and its functionality
 class VisitorHomeWindow():
@@ -35,18 +40,22 @@ class VisitorHomeWindow():
 
 		# make column IDs (cid). The "0th" column has a builtin cid "#0"
 		self.tree = ttk.Treeview(master,
-								 columns=('ID', 'Street', 'Size', 'City', 'Zip', 'Type', 'Public', 'Commercial', 'Visits', 'Avg Rating'))
+		 columns=('ID', 'Street', 'Size', 'City', 'Zip', 
+		 		  'Type', 'Public', 'Commercial', 'Visits', 'Avg Rating'))
 		self.tree.pack()
 
 		# tell Tkinter 'hey we want to see these'
-		self.tree.displaycolumns = ['ID', 'Street', 'Size', 'City', 'Zip', 'Type', 'Public', 'Commercial', 'Visits', 'Avg Rating']
+		self.tree.displaycolumns = ['ID', 'Street', 'Size', 'City', 'Zip', 
+									'Type', 'Public', 'Commercial', 'Visits', 'Avg Rating']
 
 		# set default column width
-		cols = ['#0', 'ID', 'Street', 'Size', 'City', 'Zip', 'Type', 'Public', 'Commercial', 'Visits', 'Avg Rating']
+		cols = ['#0', 'ID', 'Street', 'Size', 'City', 'Zip', 
+				'Type', 'Public', 'Commercial', 'Visits', 'Avg Rating']
 		for c in cols:
 			self.tree.column(f'{c}', width=75)
 
-		# Fill the column headers with text
+		# Fill the column headers with text.
+		# there's probably a better way to do this.
 		self.tree.heading('#0', text='Name')
 		self.tree.heading('ID', text='ID')
 		self.tree.heading('Street', text='Street')
@@ -59,33 +68,53 @@ class VisitorHomeWindow():
 		self.tree.heading('Visits', text='Visits')
 		self.tree.heading('Avg Rating', text='Avg Rating')
 
-		# see sql after class declaration
-		cursor.execute(sql)
 
-		# retrieve result of our query as tuples
-		data = cursor.fetchall()
+		def populate_table(self, sql, tree, search_val):
+		
+			# remove all info currently in the table
+			self.tree.delete(*tree.get_children())
 
-		# put our tuple results in the widget. 
-		# Important! Query return vals should be in the same order as the columns
-		count = 0 
-		for tup in data:
-
-			# Get the number of visits and their sum for every property
-			visit_stats_sql = f"SELECT COUNT(Rating), SUM(Rating) FROM Visit WHERE PropertyID={tup[1]}"
-			cursor.execute(visit_stats_sql)
-			visit_tup =  cursor.fetchone()
-
-			# avoid dividing by zero when a property has no visits
-			try:
-				visit_num, visit_avg = visit_tup[0], (visit_tup[1]/visit_tup[0])
-			except:
-				visit_num, visit_avg = visit_tup[0], "N/A"
-
-			# Fill in a row of the table
-			self.tree.insert('', count, text=tup[0], values=(tup[1], tup[2], tup[3], tup[4], tup[5], tup[6], tup[7], tup[8], visit_num, visit_avg))
-			count += 1
+			# How many children deep do we want to put
+			# each tuple of the DB? `count` many
+			count = 0 
 			
+			# This `if` runs after the user presses the `go_button`
+			# and has chosen to either:
+			# else: search by range on Visits or Avg. Rating
+			# if: done any other kind of search or sort.
+			if search_val == "":
+				
+				# Pass SQL to MySQL
+				cursor.execute(sql)
+
+				# retrieve result of our query as tuples
+				# Important! Query return vals should be 
+				# in the same order as the columns.
+				data = cursor.fetchall()
+
+				# put our tuple results in the widget. 
+				for tup in data:
+
+					# Fill in a row of the table with a tuple's values. 
+					self.tree.insert('', count, text=tup[0], values=(tup[1], tup[2], tup[3], tup[4], 
+									 tup[5], tup[6], tup[7], tup[8], tup[9], tup[10]))
+					
+					# Put the next tuple as the child of the one just processed.
+					count += 1
 			
+			# User is searching (specifying a range) on Visits or Avg. Rating.
+			else:
+
+				# Merge tuples to pair our stats to their property
+				data = cursor.fetchall()
+				print(data)
+				
+				for tup in data:
+					self.tree.insert('', count, text=tup[0], values=(tup[1], tup[2], tup[3], tup[4], 
+								 tup[5], tup[6], tup[7], tup[8], tup[9], tup[10]))
+				
+					count += 1
+
 		# buttons
 		# View Property button
 		self.view_prop_button = Button(master, text="View Selected Property", font="Times 12")
@@ -96,13 +125,13 @@ class VisitorHomeWindow():
 		self.view_visit_hist_button.pack()
 
 		# Search/Sort By drop down
-		# search_var holds the value of the drop down
-		self.search_var = StringVar(master)
+		# sort_var holds the value of the drop down
+		self.sort_var = StringVar(master)
 
 		# default value is Name
-		self.search_var.set("Name")
-		self.search_by = OptionMenu(master, self.search_var, "Name", "City", "Type", "Visits", "Avg. Rating")
-		self.search_by.pack(side=LEFT)
+		self.sort_var.set("Name")
+		self.sort_by = OptionMenu(master, self.sort_var, "Name", "City", "PropertyType", "Visits", "Avg. Rating")
+		self.sort_by.pack(side=LEFT)
 
 		# Search Input
 		self.search_entry = Entry(master)
@@ -112,12 +141,56 @@ class VisitorHomeWindow():
 		# Run the sort/search button
 		# Get values to sort and maybe search by
 		def sort_search_go(self):
-			option_val = self.search_var.get()
+
+			# Value of the dropdown sort by menu
+			option_val = self.sort_var.get()
+
+			# This is the value of the `search_entry` entry box.
 			search_val = self.search_entry.get()
-			print(option_val, search_val)
+			sql = ""
+
+			# User is specifying a search (`SELECT A FROM R WHERE B LIKE` query)
+			if search_val not in  ["", "Search"]:
+			
+				# Searching by Visits or Avg. Rating
+				if option_val in ["Visits", "Avg. Rating"]:
+					bounds = search_val.split(",")
+					print(bounds[0], bounds[1])
+					ov = "ac"
+					if option_val == "Visits":
+						ov = "cr"
+					sql = f"SELECT {ATTRS}, COUNT(Rating) AS cr, AVG(Rating) AS ar FROM Property INNER JOIN \
+							Visit ON ID=PropertyID GROUP BY Name ORDER BY {ov} HAVING {ov} > {bounds[0]}"
+
+					return populate_table(self, sql, self.tree, search_val)
+					
+				# Searching by any other DB attribute.
+				else:
+					sql = f"SELECT {ATTRS}, COUNT(Rating), AVG(Rating) FROM Property INNER JOIN \
+							Visit ON ID=PropertyID WHERE {option_val} LIKE '%{search_val}%' \
+							GROUP BY Name ORDER BY {option_val}"
+
+			# Not sorting by Visits or Avg. Rating and not searching anything.
+			# So, sorting by anything other than visits or avg rating.
+			elif option_val not in ["Visits", "Avg. Rating"]:
+				sql = f"SELECT {ATTRS}, COUNT(Rating), AVG(Rating) FROM Property INNER JOIN \
+						Visit ON ID=PropertyID GROUP BY Name ORDER BY {option_val}"
+
+			# Sorting by Avg. Rating
+			elif option_val == "Avg. Rating":
+				sql = f"SELECT {ATTRS}, COUNT(Rating), AVG(Rating) AS ar FROM Property INNER JOIN \
+						Visit ON ID=PropertyID GROUP BY Name ORDER BY ar DESC"
+			
+			# Sorting by Num Visits
+			else:
+				sql = f"SELECT {ATTRS}, COUNT(Rating) AS cr, AVG(Rating) FROM Property INNER JOIN \
+						Visit ON ID=PropertyID GROUP BY Name ORDER BY cr DESC"
+
+			return populate_table(self, sql, self.tree, search_val="")
 
 		# lambda is necessary to avoid the function being called immediately
-		self.go_button = Button(master, text="Sort/Search", command=lambda: sort_search_go(self))
+		self.go_button = Button(master, text="Sort/Search", 
+								command=lambda: sort_search_go(self))
 		self.go_button.pack(side=LEFT)
 
 		# logout
@@ -126,13 +199,17 @@ class VisitorHomeWindow():
 									font="Times 12")
 		self.logout_button.pack(side=RIGHT)
 
+		sql = f"SELECT {ATTRS}, COUNT(Rating), AVG(Rating) FROM Property INNER JOIN \
+				Visit ON ID=PropertyID GROUP BY Name"
+		populate_table(self, sql, self.tree, search_val="")
 		
 db = sql.connect(host="academic-mysql.cc.gatech.edu",
 					 user="cs4400_team_26",
 					 passwd="YFxUWSqD",
 					 db="cs4400_team_26")
+
+# We give this to the window instance
 cursor = db.cursor()
-sql = "SELECT Name, ID, Street, Size, City, Zip, PropertyType, IsPublic, IsCommercial FROM Property"
 
 # make Tk instance
 root = Tk()
