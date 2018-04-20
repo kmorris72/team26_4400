de35866d7b5cb6834c100e7312165eff730a56b5
kmorris72 <kipp.morris@gmail.com>
2018-04-19 21:34:41 -0400

    Added searching to AdminVisitorOverviewWindow

--- AdminVisitorOverviewWindow.py
+++ AdminVisitorOverviewWindow.py
@@ -36,7 +36,8 @@ class AdminVisitorOverviewWindow(Frame):
 
         self.delete_visitor_button = Button(self.delete_back_button_container,
                                          text="Delete Visitor Account",
-                                         padx=10)
+                                         padx=10,
+                                         command=self.delete_visitor_button_clicked_handler)
         self.delete_visitor_button.pack(pady=(0, 20))
 
         self.delete_log_button = Button(self.delete_back_button_container,
@@ -46,7 +47,8 @@ class AdminVisitorOverviewWindow(Frame):
 
         self.back_button = Button(self.delete_back_button_container,
                                   text="Back",
-                                  padx=10)
+                                  padx=10,
+                                  command=self.back_button_clicked_handler)
         self.back_button.pack()
 
         self.search_container = Frame(self.button_container)
@@ -66,9 +68,42 @@ class AdminVisitorOverviewWindow(Frame):
         self.search_text.pack(side=TOP, pady=(0, 10))
 
         self.search_button = Button(self.search_container,
-                                    text="Search Properties",
-                                    padx=10)
+                                    text="Search Visitors",
+                                    padx=10,
+                                    command=self.search_button_clicked_handler)
         self.search_button.pack(side=TOP)
+
+
+    def delete_visitor_button_clicked_handler(self):
+        table_item = self.table.focus()
+        visitor_username = self.table.item(table_item)["values"][0]
+        delete_query = """DELETE FROM User
+                          WHERE Username=\"{}\"""".format(visitor_username)
+        self.db_cursor.execute(delete_query)
+        self.table.delete(table_item)
+
+    
+    def search_button_clicked_handler(self):
+        search_attr = self.search_by_var.get()
+        search_val = self.search_text.get()
+        if search_attr == COLUMN_NAMES[0] or search_attr == COLUMN_NAMES[1]:
+            self.populate_table("""SELECT U.Username, Email, COUNT(*) 
+                                   FROM User AS U LEFT OUTER JOIN Visit as V
+                                   ON U.Username=V.Username
+                                   WHERE U.UserType="VISITOR" AND U.{}=\"{}\"
+                                   GROUP BY U.Username""".format(search_attr, search_val))
+        else:
+            self.populate_table("""SELECT U.Username, Email, COUNT(*) as VisitCount
+                                   FROM User AS U LEFT OUTER JOIN Visit as V
+                                   ON U.Username=V.Username
+                                   WHERE U.UserType="VISITOR"
+                                   GROUP BY U.Username
+                                   HAVING VisitCount={}""".format(search_val))
+
+
+    def back_button_clicked_handler(self):
+        # self.master.master.show_window()
+        return
 
 
     def populate_table(self, query):
