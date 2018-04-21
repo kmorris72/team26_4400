112080497f2e47e83bec5009c35ba1e6571fd2a3
IrL1 <37993909+IrL1@users.noreply.github.com>
2018-04-21 17:14:19 -0400

    Update adminmanageproperty.py

--- adminmanageproperty.py
+++ adminmanageproperty.py
@@ -2,217 +2,251 @@ from tkinter import *
 from tkinter import ttk
 
 class adminmanagepropertywindow:
-    def __init__(self, master):
+    def __init__(self, master, db_cursor):
         self.master = master
         master.title("Unconfirmed Properties")
 
+        self.db_cursor=db_cursor
+
+
+        name="""select name from propertylist"""
         self.label = Label(self.master,
                            text="Manage Property",
                            font="Times 32")
         self.label.pack()
 
-        self.container1=Frame(master)
-        self.container1.pack()
+        self.basicinfocontainer=Frame(master)
+        self.basicinfocontainer.pack()
 
-        self.container1a=Frame(self.container1)
-        self.container1a.pack(side=LEFT)
+        self.basicinfocontainerlabelspart1=Frame(self.basicinfocontainer)
+        self.basicinfocontainerlabelspart1.pack(side=LEFT)
 
-        self.container1aa=Frame(self.container1a)
-        self.container1aa.pack(side=LEFT)
+        self.basicinfocontainerlabelspart1leftside=Frame(self.basicinfocontainerlabelspart1)
+        self.basicinfocontainerlabelspart1leftside.pack(side=LEFT)
 
-        self.namelabel = Label(self.container1aa,
+        self.namelabel = Label(self.basicinfocontainerlabelspart1leftside,
                                text="Name:",
                                font="Times 12")
         self.namelabel.pack()
 
-        self.addlabel = Label(self.container1aa,
+        self.addlabel = Label(self.basicinfocontainerlabelspart1leftside,
                            text="Address:",
                            font="Times 12")
         self.addlabel.pack()
 
-        self.citlabel = Label(self.container1aa,
+        self.citlabel = Label(self.basicinfocontainerlabelspart1leftside,
                            text="City:",
                            font="Times 12")
         self.citlabel.pack()
 
-        self.ziplabel = Label(self.container1aa,
+        self.ziplabel = Label(self.basicinfocontainerlabelspart1leftside,
                            text="Zip:",
                            font="Times 12")
         self.ziplabel.pack()
 
-        self.sizlabel = Label(self.container1aa,
+        self.sizlabel = Label(self.basicinfocontainerlabelspart1leftside,
                            text="Size (acres):",
                            font="Times 12")
         self.sizlabel.pack()
 
-        self.container1ab=Frame(self.container1a)
-        self.container1ab.pack(side=RIGHT)
+        self.basicinfocontainerlabelspart1rightside=Frame(self.basicinfocontainerlabelspart1)
+        self.basicinfocontainerlabelspart1rightside.pack(side=RIGHT)
 
-        self.nameentry = Entry(self.container1ab,
+        self.nameentry = Entry(self.basicinfocontainerlabelspart1rightside,
                                 font="Times 12",
                                 width=12)
         self.nameentry.pack()
 
-        self.addressentry = Entry(self.container1ab,
+        self.addressentry = Entry(self.basicinfocontainerlabelspart1rightside,
                                 font="Times 12",
                                 width=12)
         self.addressentry.pack()
 
-        self.cityentry = Entry(self.container1ab,
+        self.cityentry = Entry(self.basicinfocontainerlabelspart1rightside,
                                 font="Times 12",
                                 width=12)
         self.cityentry.pack()
 
-        self.zipentry = Entry(self.container1ab,
+        self.zipentry = Entry(self.basicinfocontainerlabelspart1rightside,
                                 font="Times 12",
                                 width=12)
         self.zipentry.pack()
 
-        self.sizeentry = Entry(self.container1ab,
+        self.sizeentry = Entry(self.basicinfocontainerlabelspart1rightside,
                                 font="Times 12",
                                 width=12)
         self.sizeentry.pack()
 
 
-        self.container1b=Frame(self.container1)
-        self.container1b.pack()
+        self.basicinfocontainerlabelspart2=Frame(self.basicinfocontainerlabels)
+        self.basicinfocontainerlabelspart2.pack()
 
-        self.container1ba=Frame(self.container1b)
-        self.container1ba.pack(side=LEFT)
+        self.basicinfocontainerlabelspart2leftside=Frame(self.basicinfocontainerlabelspart2)
+        self.basicinfocontainerlabelspart2leftside.pack(side=LEFT)
 
-        self.typlabel = Label(self.container1ba,
+        self.typlabel = Label(self.basicinfocontainerlabelspart2leftside,
                            text="Type:",
                            font="Times 12")
         self.typlabel.pack()
 
-        self.publabel = Label(self.container1ba,
+        self.publabel = Label(self.basicinfocontainerlabelspart2leftside,
                            text="Public:",
                            font="Times 12")
         self.publabel.pack()
 
-        self.comlabel = Label(self.container1ba,
+        self.comlabel = Label(self.basicinfocontainerlabelspart2leftside,
                            text="Commercial:",
                            font="Times 12")
         self.comlabel.pack()
 
-        self.idlabel = Label(self.container1ba,
+        self.idlabel = Label(self.basicinfocontainerlabelspart2leftside,
                            text="ID:",
                            font="Times 12")
         self.idlabel.pack()
 
-        self.container1bb=Frame(self.container1b)
-        self.container1bb.pack(side=RIGHT)
+        self.basicinfocontainerlabelspart2rightside=Frame(self.basicinfocontainerlabelspart2)
+        self.basicinfocontainerlabelspart2rightside.pack(side=RIGHT)
 
-        self.atypelabel = Label(self.container1bb,
+        self.atypelabel = Label(self.basicinfocontainerlabelspart2rightside,
                            text="Farm",
                            font="Times 12")
         self.atypelabel.pack()
 
-        self.tkvar2 = StringVar(self.container1bb)
+        self.tkvar2 = StringVar(self.basicinfocontainerlabelspart2rightside)
         choices = { 'True','False'}
         self.tkvar2.set('False')
 
-        self.popupMenu2 = OptionMenu(self.container1bb, self.tkvar2, *choices)
+        self.popupMenu2 = OptionMenu(self.basicinfocontainerlabelspart2rightside, self.tkvar2, *choices)
         self.popupMenu2.pack()
 
 
-        self.tkvar1 = StringVar(self.container1bb)
+        self.tkvar1 = StringVar(self.basicinfocontainerlabelspart2rightside)
         choices = { 'True','False'}
         self.tkvar1.set('False')
 
-        self.popupMenu1 = OptionMenu(self.container1bb, self.tkvar1, *choices)
+        self.popupMenu1 = OptionMenu(self.basicinfocontainerlabelspart2rightside, self.tkvar1, *choices)
         self.popupMenu1.pack()
 
 
-        self.idnumlabel = Label(self.container1bb,
+        self.idnumlabel = Label(self.basicinfocontainerlabelspart2rightside,
                            text="ID:",
                            font="Times 12")
         self.idnumlabel.pack()
 
 
 
-        self.container2=Frame(master)
-        self.container2.pack()
+        self.animalsandcropscontainer=Frame(master)
+        self.animalsandcropscontainer.pack()
 
-        self.container2a=Frame(self.container2)
-        self.container2a.pack(side=LEFT)
+        self.animalsandcropscontainerpartanimals=Frame(self.animalsandcropscontainer)
+        self.animalsandcropscontainerpartanimals.pack(side=LEFT)
 
-        self.container2aa=Frame(self.container2a)
-        self.container2aa.pack(side=LEFT)
+        self.animalsandcropscontainerpartanimalslabelsside=Frame(self.animalsandcropscontainerpartanimals)
+        self.animalsandcropscontainerpartanimalslabelsside.pack(side=LEFT)
 
-        self.container2ab=Frame(self.container2a)
-        self.container2ab.pack(side=RIGHT)
-
-        self.container2b=Frame(self.container2)
-        self.container2b.pack(side=RIGHT)
-
-        self.container2ba=Frame(self.container2b)
-        self.container2ba.pack(side=LEFT)
-
-        self.container2bb=Frame(self.container2b)
-        self.container2bb.pack(side=RIGHT)
+        self.alabel=Label(self.animalsandcropscontainerpartanimalslabelsside,
+                        text="Animals:",
+                        font="Times 12")
 
 
-        self.container3=Frame(master)
-        self.container3.pack()
+        self.animalsandcropscontainerpartanimalslistside=Frame(self.animalsandcropscontainerpartanimals)
+        self.animalsandcropscontainerpartanimalslistside.pack(side=RIGHT)
 
-        self.container3a=Frame(self.container3)
-        self.container3a.pack(side=LEFT)
+        self.animalsandcropscontainercropsside=Frame(self.animalsandcropscontainer)
+        self.animalsandcropscontainercropsside.pack(side=RIGHT)
 
-        self.animlabel = Label(self.container3a,
+        self.animalsandcropscontainercropssidelabelside=Frame(self.animalsandcropscontainercropsside)
+        self.animalsandcropscontainercropssidelabelside.pack(side=LEFT)
+
+        self.alabel=Label(self.animalsandcropscontainercropssidelabelside,
+                        text="Crops:",
+                        font="Times 12")
+
+
+        self.animalsandcropscontainercropssidelistsside=Frame(self.animalsandcropscontainercropsside)
+        self.animalsandcropscontainercropssidelistsside.pack(side=RIGHT)
+
+
+        self.addanimalsorcropscontainer=Frame(master)
+        self.addanimalsorcropscontainer.pack()
+
+        self.addanimalsorcropscontaineraddanimal=Frame(self.addanimalsorcropscontainer)
+        self.addanimalsorcropscontaineraddanimal.pack(side=LEFT)
+
+        self.animlabel = Label(self.addanimalsorcropscontaineraddanimal,
                            text="Add new Animal:",
                            font="Times 12")
         self.animlabel.pack(side=LEFT, pady=(50,0))
 
-        self.container3ab=Frame(self.container3a)
-        self.container3ab.pack(side=RIGHT)
+        self.addanimalsorcropscontaineraddanimalselectside=Frame(self.addanimalsorcropscontaineraddanimal)
+        self.addanimalsorcropscontaineraddanimalselectside.pack(side=RIGHT)
 
-        self.addan_button = Button(self.container3ab,
+
+        self.tkvar = StringVar(self.addanimalsorcropscontaineraddanimalselectside)
+
+        choices = { 'Select approved animal...','Fruit','Animal','Vegetable','Flower'}
+        self.tkvar.set('Select approved animal...')
+
+        self.popupMenu = OptionMenu(self.addanimalsorcropscontaineraddanimalselectside, self.tkvar, *choices)
+        self.popupMenu.pack()
+
+
+        self.addan_button = Button(self.addanimalsorcropscontaineraddanimalselectside,
                                    text="Add Animal to \nProperty",
                                    padx=10,
                                    height=2,width=12)
-        self.addan_button.pack(side=LEFT, pady=(55,10),padx=5)
+        self.addan_button.pack(side=LEFT, pady=(5,10),padx=5)
 
 
 
-        self.container3b=Frame(self.container3)
-        self.container3b.pack(side=RIGHT)
+        self.addanimalsorcropscontaineraddcrops=Frame(self.addanimalsorcropscontainer)
+        self.addanimalsorcropscontaineraddcrops.pack(side=RIGHT)
 
-        self.animlabel = Label(self.container3b,
+        self.animlabel = Label(self.addanimalsorcropscontaineraddcrops,
                            text="Add new Crop:",
                            font="Times 12")
         self.animlabel.pack(side=LEFT, pady=(50,0))
 
-        self.container3bb=Frame(self.container3b)
-        self.container3bb.pack(side=RIGHT)
+        self.addanimalsorcropscontaineraddcropsselectside=Frame(self.addanimalsorcropscontaineraddcrops)
+        self.addanimalsorcropscontaineraddcropsselectside.pack(side=RIGHT)
 
-        self.addcr_button = Button(self.container3bb,
+        self.tkvar2 = StringVar(self.addanimalsorcropscontaineraddcropsselectside)
+
+        choices = { 'Select approved crop...','Name',"Type"}
+        self.tkvar2.set('Select approved crop...')
+
+        self.popupMenu2 = OptionMenu(self.addanimalsorcropscontaineraddcropsselectside, self.tkvar2, *choices)
+        self.popupMenu2.pack()
+
+
+
+        self.addcr_button = Button(self.addanimalsorcropscontaineraddcropsselectside,
                                    text="Add Crop to \nProperty",
                                    padx=10,
                                    height=2,width=12)
-        self.addcr_button.pack(side=LEFT, pady=(55,10),padx=5)
+        self.addcr_button.pack(side=LEFT, pady=(5,10),padx=5)
 
 
-        self.container4=Frame(master)
-        self.container4.pack()
+        self.deletesavebackbuttonscontainer=Frame(master)
+        self.deletesavebackbuttonscontainer.pack()
 
 
-        self.delete_property_button = Button(self.container4,
+        self.delete_property_button = Button(self.deletesavebackbuttonscontainer,
                                    text="Delete Property",
                                    padx=10,
                                    width = 12)
         self.delete_property_button.pack(side=LEFT, pady=(75,0))
 
-        self.container4b=Frame(self.container4)
-        self.container4b.pack(side=RIGHT)
+        self.deletesavebackbuttonscontainermiddlesection=Frame(self.deletesavebackbuttonscontainer)
+        self.deletesavebackbuttonscontainermiddlesection.pack(side=RIGHT)
 
-        self.save_button = Button(self.container4b,
+        self.save_button = Button(self.deletesavebackbuttonscontainermiddlesection,
                                    text="Save Changes\n(confirm property)",
                                    padx=10,
                                    height=3,width=15)
         self.save_button.pack()
 
-        self.back_button = Button(self.container4b,
+        self.back_button = Button(self.deletesavebackbuttonscontainermiddlesection,
                                    text="Back\n(Don't Save or Confirm)",
                                    padx=10,
                                    height=3,width=15)
@@ -226,3 +260,4 @@ class adminmanagepropertywindow:
 root = Tk()
 my_gui = adminmanagepropertywindow(root)
 root.mainloop()
+
