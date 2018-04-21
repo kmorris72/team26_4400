a1d0480647b5f15793c98a802f2be3181c341efd
JPadd <38011144+JPadd@users.noreply.github.com>
2018-04-21 15:59:24 -0400

    Add files via upload

--- AddNewPropertyWindow.py
+++ AddNewPropertyWindow.py
@@ -1,16 +1,18 @@
 #NOTES:
-#1. Still need to add definition of username for SQL queries
-#2. Should anything be added to distinguish crop types for farms vs orchards in the code?
+#Stuff to do
+#1. Pass in of username
+#2. Path for Add New Property button. Where to go after property is added?
+#3. Path for cancel button
 
 from tkinter import *
 import tkinter.messagebox as messagebox
 import MySQLdb as sql
 
-db = sql.connect(host="academic-mysql.cc.gatech.edu",
-				user="cs4400_team_26", passwd="YFxUWSqD",
-				db="cs4400_team_26")
+#db = sql.connect(host="academic-mysql.cc.gatech.edu",
+#				user="cs4400_team_26", passwd="YFxUWSqD",
+#				db="cs4400_team_26")
 
-root = Tk()
+#root = Tk()
 
 # The options for the property type drop down.
 PROP_TYPES = ["Farm", "Orchard", "Garden"]
@@ -19,49 +21,51 @@ PUB_COMM_VALUES = ["Yes", "No"]
 # The two types of farm items.
 ITEM_TYPES = ["Crop", "Animal"]
 
-class AddNewPropertyWindow:
-	def __init__(self, master):
-		self.db_cursor = db.cursor()
-		self.master = master
-		master.title("Add New Property")
+class AddNewPropertyWindow(Frame):
+	def __init__(self, parent, db_cursor):
+		Frame.__init__(self, parent)
 
-		Label(master, text="Add New Property", font="Times 36"). grid(row=0, columnspan=6)
+		self.db_cursor = db_cursor
+		#self.self = self
+		#self.title("Add New Property")
+
+		Label(self, text="Add New Property", font="Times 36"). grid(row=0, columnspan=6)
 		
-		Label(master, text="Property Name:", font="Times 12").grid(row=1,column=0, sticky=E, padx=0)
-		self.propertyName_entry = Entry(master, )#width=40)
+		Label(self, text="Property Name:", font="Times 12").grid(row=1,column=0, sticky=E, padx=0)
+		self.propertyName_entry = Entry(self, )#width=40)
 		self.propertyName_entry.grid(row=1,column=1, columnspan=5, sticky=W+E)
 		
-		Label(master, text="Street Address:", font="Times 12").grid(row=2,column=0, sticky=E)
-		self.address_entry = Entry(master, )#width=40)
+		Label(self, text="Street Address:", font="Times 12").grid(row=2,column=0, sticky=E)
+		self.address_entry = Entry(self, )#width=40)
 		self.address_entry.grid(row=2,column=1, columnspan=5, sticky=W+E)
 
-		Label(master, text="City:", font="Times 12").grid(row=3, column=0, sticky=E)
-		self.city_entry = Entry(master, )#width=15)
+		Label(self, text="City:", font="Times 12").grid(row=3, column=0, sticky=E)
+		self.city_entry = Entry(self, )#width=15)
 		self.city_entry.grid(row=3, column=1, columnspan=2, sticky=W+E)
 		
-		Label(master, text="Zip:", font="Times 12").grid(row=4, column=0, sticky=E)
-		self.zip_entry = Entry(master, )#width=10)
+		Label(self, text="Zip:", font="Times 12").grid(row=4, column=0, sticky=E)
+		self.zip_entry = Entry(self, )#width=10)
 		self.zip_entry.grid(row=4, column=1, columnspan=2, sticky=W+E)
 
-		Label(master, text="Acres:", font="Times 12").grid(row=5, column=0, sticky=E)
-		self.acres_entry = Entry(master, )#width=10)
+		Label(self, text="Acres:", font="Times 12").grid(row=5, column=0, sticky=E)
+		self.acres_entry = Entry(self, )#width=10)
 		self.acres_entry.grid(row=5,column=1,columnspan=2, sticky=W+E)
 
-		self.prop_type_var = StringVar(master)
+		self.prop_type_var = StringVar(self)
 		#self.prop_type_var.set(PROP_TYPES[0])
 		self.prop_type_var.trace("w", self.prop_type_changed_event_handler)
-		Label(master, text="Property Type:", font="Times 12").grid(row=6, column=0, sticky=E)
-		self.propertyType_entry = OptionMenu(master, self.prop_type_var, "Farm", "Garden", "Orchard")
+		Label(self, text="Property Type:", font="Times 12").grid(row=6, column=0, sticky=E)
+		self.propertyType_entry = OptionMenu(self, self.prop_type_var, "Farm", "Garden", "Orchard")
 		self.propertyType_entry.grid(row=6, column=1, sticky=W)
 
 		animal_query = "SELECT Name FROM FarmItem WHERE Type=\"Animal\""
-		self.animal = StringVar(master)
+		self.animal = StringVar(self)
 		self.db_cursor.execute(animal_query)
 		animal_list = list(self.db_cursor.fetchall())
 		for i in range(len(animal_list)):
 			animal_list[i] = animal_list[i][0].strip("{").strip("}").strip()
-		Label(master, text="Animal:", font="Times 12").grid(row=6, column=2, sticky=E)
-		self.animal_entry = OptionMenu(master, self.animal, *animal_list)#list of animals as strings. can maybe pull from db...
+		Label(self, text="Animal:", font="Times 12").grid(row=6, column=2, sticky=E)
+		self.animal_entry = OptionMenu(self, self.animal, *animal_list)#list of animals as strings. can maybe pull from db...
 		#self.animal_entry.grid(row=6, column=3, sticky=W)
 
 		crop_query = "SELECT Name FROM FarmItem WHERE Type<>\"Animal\""
@@ -69,27 +73,27 @@ class AddNewPropertyWindow:
 		crop_list = list(self.db_cursor.fetchall())
 		for i in range(len(crop_list)):
 			crop_list[i] = crop_list[i][0].strip("{").strip("}").strip()
-		self.crop = StringVar(master)
-		Label(master, text="Crop:", font="Times 12").grid(row=6, column=4, sticky=E)
-		self.crop_entry = OptionMenu(master, self.crop, *crop_list)#list of crops as strings)
+		self.crop = StringVar(self)
+		Label(self, text="Crop:", font="Times 12").grid(row=6, column=4, sticky=E)
+		self.crop_entry = OptionMenu(self, self.crop, *crop_list)#list of crops as strings)
 		self.crop_entry.grid(row=6, column=5, sticky=W)
 
-		self.public = StringVar(master)
-		Label(master, text="Public?", font="Times 12").grid(row=7, column=0, sticky=E)
-		self.public_entry= OptionMenu(master, self.public, "Yes", "No")
+		self.public = StringVar(self)
+		Label(self, text="Public?", font="Times 12").grid(row=7, column=0, sticky=E)
+		self.public_entry= OptionMenu(self, self.public, "Yes", "No")
 		self.public_entry.grid(row=7, column=1, sticky=W)
 
-		self.commercial = StringVar(master)
-		Label(master, text="Commerical?", font="Times 12").grid(row=8, column=0, sticky=E)
-		self.commercial_entry= OptionMenu(master, self.commercial, "Yes", "No")
+		self.commercial = StringVar(self)
+		Label(self, text="Commerical?", font="Times 12").grid(row=8, column=0, sticky=E)
+		self.commercial_entry= OptionMenu(self, self.commercial, "Yes", "No")
 		self.commercial_entry.grid(row=8, column=1, sticky=W)
 
-		self.addPropButton = Button(master, text="Add Property",
+		self.addPropButton = Button(self, text="Add Property",
 								command=self.add_prop_event_handler)
 		self.addPropButton.grid(row=9,column=1, sticky=W+E,columnspan=4)
 		#addPropButton.bind("<Button-1>", self.add_prop_event_handler)
 
-		self.cancelButton = Button(master, text="Cancel",
+		self.cancelButton = Button(self, text="Cancel",
 									command=self.cancel_event_handler)
 		self.cancelButton.grid(row=9,column=5, columnspan=1, sticky=W+E)
 
@@ -102,7 +106,7 @@ class AddNewPropertyWindow:
 			self.animal_entry.grid_forget()
 
 	def cancel_event_handler(self):
-			root.destroy()
+			print(self.master.master.logged_in_user)
 	def add_prop_event_handler(self):
 		no_empty_text = True 
 		for entry in (self.propertyName_entry, self.address_entry, self.city_entry, self.zip_entry, 
@@ -172,7 +176,7 @@ class AddNewPropertyWindow:
 		prop_street = self.address_entry.get().strip()
 		prop_city = self.city_entry.get().strip()
 		prop_type = self.prop_type_var.get().upper()
-		username = ""
+		username = self.master.parent.logged_in_user
 		prop_insert_query = "INSERT INTO Property VALUES ({}, \"{}\", {}, {}, {}, \"{}\", \"{}\", {}, \"{}\", \"{}\", NULL)".format(prop_id, prop_name, prop_size, is_commercial, is_public, prop_street, prop_city, prop_zip, prop_type, username)
 		self.db_cursor.execute(prop_insert_query)
 
@@ -194,6 +198,6 @@ class AddNewPropertyWindow:
 			messagebox.showinfo("Success!", "Your property has been added!")
 
 
-my_gui = AddNewPropertyWindow(root)
-root.mainloop()
+#my_gui = AddNewPropertyWindow(root)
+#root.mainloop()
 
